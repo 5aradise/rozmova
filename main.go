7d1db373704b51ae53ddcc3adcba9d9d4ebb5204
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-04-07 02:04:35 +0300

    add welcome page, logo and readiness endpoint

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
+	const addrespathRoot = "/app"
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", healthz)
+	mux.Handle(addrespathRoot+"/*", http.StripPrefix(addrespathRoot, http.FileServer(http.Dir(filepathRoot))))
+
 	corsMux := middlewareCors(mux)
 
 	err := http.ListenAndServe(":"+port, corsMux)
@@ -18,15 +24,8 @@ func main() {
 	}
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`Status: OK`))
 }
