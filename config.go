908babb5a633e36eeb556875be111a516503eaeb
5aradise <rozumovskyi.daniil@lll.kpi.ua>
2024-04-07 03:06:56 +0300

    add config and api

---
+++ config.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func NewApiConfig() *apiConfig {
+	return &apiConfig{
+		fileserverHits: 0,
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		h.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics reset"))
+}
+
+func (cfg *apiConfig) showMetrics(w http.ResponseWriter, r *http.Request) {
+	metrics := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(metrics))
+}
--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 
 func main() {
 	const filepathRoot = "."
-	const addrespathRoot = "/app"
+	const appPathRoot = "/app"
+	const apiPathRoot = "/api"
 	const port = "8080"
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", healthz)
-	mux.Handle(addrespathRoot+"/*", http.StripPrefix(addrespathRoot, http.FileServer(http.Dir(filepathRoot))))
+	cfg := NewApiConfig()
+
+	mux.HandleFunc("GET "+apiPathRoot+"/healthz", healthz)
+	mux.HandleFunc("GET "+apiPathRoot+"/metrics", cfg.showMetrics)
+	mux.HandleFunc(""+apiPathRoot+"/reset", cfg.resetHits)
+	mux.Handle(""+appPathRoot+"/*", cfg.middlewareMetricsInc(http.StripPrefix(appPathRoot, http.FileServer(http.Dir(filepathRoot)))))
 
 	corsMux := middlewareCors(mux)
 
@@ -22,10 +27,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func healthz(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`Status: OK`))
 }
